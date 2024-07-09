ccad00ce6d6803ecc044528754d18904e308489d
Om-Govindani <omgovindani@gmail.com>
2024-07-10 04:23:04 +0530

    Added Profile button and SearchBar in NavBar

--- frontend/src/components/PasswordInput.jsx
+++ frontend/src/components/PasswordInput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import {FaRegEye ,FaRegEyeSlash} from "react-icons/fa6"
 
 
-const PasswordInput = ({value,onchange,placeholder})=>{
+const PasswordInput = ({value,onChange,placeholder})=>{
     const [isShowPassword ,setIsShowPassword] = useState(false);
     const toggleShowPassword = ()=>{
         setIsShowPassword(!isShowPassword)
@@ -11,7 +11,7 @@ const PasswordInput = ({value,onchange,placeholder})=>{
         <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
             <input
                 value={value}
-                onChange={onchange}
+                onChange={onChange}
                 type={isShowPassword?"text":"password"}
                 placeholder={placeholder || "Enter Password"}
                 className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none" 
